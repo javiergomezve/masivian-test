87944a457c372d71cba7a071fdfed09f00c0f1f7
Javier Gomez <javiergomezve@gmail.com>
2021-03-19 12:55:10 -0500

    Finish app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,141 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <header class="header">
+            <h1 class="header__title">Comics</h1>
+        </header>
+
+        <section class="content">
+            <p v-if="isLoading">Cargando datos...</p>
+            <Comic
+                v-else
+                :title="title"
+                :image-url="imageUrl"
+                :image-alt="imageAlt"
+                :comic-number="comicNumber"
+                :date="date"
+                :rating="rating"
+                :set-rating="setRating"
+            />
+        </section>
+
+        <footer class="footer">
+            <p class="footer_content">
+                Build by <a href="https://github.com/javiergomezve" target="_blank">javiergomezve</a> for <a target="_blank" href="https://masiv.com/">masivian</a>
+            </p>
+        </footer>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Comic from './components/Comic.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Comic
+    },
+    data() {
+        return {
+            isLoading: false,
+            title: '',
+            imageUrl: '',
+            imageAlt: '',
+            comicNumber: 0,
+            date: '',
+            rating: 0,
+        };
+    },
+    computed: {
+        comic_storage: function () {
+            return `masivian_comic_${this.comicNumber}`;
+        }
+    },
+    mounted() {
+        this.fetchComic();
+    },
+    methods: {
+        setRating(rating) {
+            const vm = this;
+            localStorage.setItem(vm.comic_storage, rating);
+        },
+
+        getRating() {
+            const vm = this;
+            const rate = localStorage.getItem(vm.comic_storage);
+
+            if (rate) {
+                vm.rating = parseInt(rate);
+            }
+        },
+
+        fetchComic() {
+            const vm = this;
+
+            vm.isLoading = true;
+            vm.axios.get('https://masivia-back-test.herokuapp.com/api/comic')
+                .then(function(response) {
+                    const comic = response.data;
+                    vm.title = comic.title;
+                    vm.imageAlt = comic.alt;
+                    vm.imageUrl = comic.img;
+                    vm.comicNumber = comic.num;
+                    vm.date = `${comic.month}/${comic.day}/${comic.year}`;
+
+                    vm.getRating();
+
+                    vm.isLoading = false;
+                })
+                .catch(function (error) {
+                    console.error(error);
+                });
+        },
+    }
 }
 </script>
 
 <style lang="scss">
+html {
+    height: 100%;
+}
+
+body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    background-color: #e3e3e3;
+    color: #fff;
+    height: 100%;
+    font-family: 'Comic Neue', cursive;
+}
+
+a {
+    color: #00AFF4;
+    text-decoration: none;
+
+    &:hover {
+        text-decoration: underline;
+    }
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+
+    .header, .footer {
+        padding: 10px 0;
+        width: 100%;
+        background: #0C1F55;
+        text-align: center;
+    }
+
+    .content {
+        flex: 1;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 }
 </style>
---
+++ src/components/Comic.vue
@@ -0,0 +1,77 @@
+<template>
+    <article class="comic">
+        <h2 class="comic__title">{{title}}</h2>
+        <img class="comic__image" v-bind:src="imageUrl" v-bind:alt="imageAlt" />
+        <div class="comic__meta">
+            <p class="comic__number">Comic #{{ comicNumber }}</p>
+            <p class="comic__date">Date {{ date }}</p>
+        </div>
+        <div class="comic_classification">
+            <star-rating
+                v-bind:star-size="20"
+                inactive-color="#e3e3e3"
+                v-bind:rating="rating"
+                @rating-selected ="setRating"
+            />
+        </div>
+    </article>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating'
+
+export default {
+    name: 'Comic',
+    components: {
+        StarRating
+    },
+    props: {
+        title: String,
+        imageUrl: String,
+        imageAlt: String,
+        comicNumber: Number,
+        date: String,
+        rating: Number,
+        setRating: Function,
+    }
+}
+</script>
+
+<style lang="scss">
+.comic {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    background: #70E2BF;
+    padding: 10px 20px;
+
+    &__title {
+        margin: 0 0 10px;
+    }
+
+    &__image {
+        width: 100%;
+    }
+
+    &__meta {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+        align-items: center;
+        width: 100%;
+    }
+
+    &__number, &__date {
+        margin: 10px 0 0;
+        flex: 1;
+        text-align: center;
+    }
+
+    &_classification {
+        .vue-star-rating {
+            margin: 10px 0 5px;
+            height: 1rem;
+        }
+    }
+}
+</style>
